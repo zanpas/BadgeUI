aaa42ecfbd8eeacff884ff3f68f476556412d054
zanpas <pasquale.zannino@gmail.com>
2016-08-03 17:21:35 +0200

    First Commit

---
+++ www/js/controllers/BadgeCtrl.js
@@ -0,0 +1,102 @@
+﻿app.controller('BadgeCtrl', function ($scope, $rootScope ,$ionicPopup, $timeout) {
+
+    $scope.started = false;
+    $scope.pranzo = false;
+    $scope.max = false;
+    $scope.permesso = false;
+    $scope.date = new Date();
+
+    $scope.start = function(){
+        $scope.started = true;
+        $scope.entrata = new Date();
+        $scope.entrataOra = $scope.entrata.getHours();
+        $scope.entrataMinuti = $scope.entrata.getMinutes();
+        console.log($scope.entrataOra , $scope.entrataMinuti);
+    };
+
+    $scope.ripresaPausa = function(){
+        $scope.pranzo = false;
+        $scope.started = true;
+    }
+
+    $scope.finePausa = function(){
+        $scope.pranzo = false;
+        $scope.started = false;
+    }
+
+    $scope.stop = function (row) {
+        $scope.uscita = new Date();
+        $scope.uscitaOra = $scope.uscita.getHours();
+        $scope.uscitaMinuti = $scope.uscita.getMinutes();
+        console.log($scope.uscitaOra , $scope.uscitaMinuti);
+        $scope.newRow = {};
+
+        /* if($scope.profileRow.profile == 'user') {
+         $scope.other3 = 'admin';
+         }*/
+
+        var myPopup =  $ionicPopup.show({
+            templateUrl: 'templates/badgePopup.html',
+            title: 'Opzioni disponibili',
+            subTitle: 'Azione da prendere',
+            scope: $scope,
+            buttons: [
+                { text: 'Annulla' },
+                {
+                    text: '<b>Conferma</b>',
+                    type: 'button-positive',
+                    onTap: function(e) {
+                        if($scope.newRow.event != null) {
+                            e.preventDefault();
+                            $scope.started = false;
+                            if($scope.uscitaMinuti<$scope.entrataMinuti) {
+                                $scope.parzialeOre=$scope.uscitaOra-$scope.entrataOra;
+                                $scope.totaleOre=$scope.parzialeOre-1;
+                                $scope.parzialeMinuti=60-$scope.entrataMinuti;
+                                $scope.totaleMinuti=$scope.parzialeMinuti+$scope.uscitaMinuti
+                            }
+                            else {
+                                $scope.totaleOre=$scope.uscitaOra-$scope.entrataOra;
+                                $scope.totaleMinuti=$scope.uscitaMinuti-$scope.entrataMinuti;
+                            }
+                            console.log($scope.totaleOre , $scope.totaleMinuti ,$scope.newRow.event);
+                            if($scope.newRow.event == "Pausa pranzo") {
+                                $scope.pranzo = true;
+                                $scope.max = true;
+                            }
+                            if($scope.newRow.event == "Richiesta permesso") {
+                                $scope.permesso = true;
+                                var alertPopup = $ionicPopup.alert({
+                                    title: 'Hai richiesto un permesso',
+                                    templateUrl: 'templates/badgePermessoOrePopup.html'
+                                });
+                            }
+                            if($scope.newRow.event == "Fine giornata lavorativa") {
+                                var alertPopup = $ionicPopup.alert({
+                                    title: 'Scegli dove scaricare le ore',
+                                    templateUrl: 'templates/badgeScaricoOrePopup.html'
+                                });
+                            }
+                            myPopup.close();
+                        }
+                        else {
+                            var alertPopup = $ionicPopup.alert({
+                                title: 'Alert',
+                                template: 'Not possible'
+                            });
+                            alertPopup.then(function(res) {
+                                // Custom functionality....
+                            });
+                        }
+                    }
+                }
+            ]
+        });
+
+        $timeout(function() {
+            myPopup.close(); //close the popup after 30 seconds for some reason
+        }, 30000);
+    };
+
+
+});
---
+++ www/js/controllers/ProfilaCtrl.js
@@ -0,0 +1,121 @@
+﻿app.controller('ProfilaCtrl', function ($scope, $rootScope, $ionicPopup, $timeout, $http) {
+
+    $scope.exportProfileExcel = function () {
+        var blob = new Blob([document.getElementById('exportableProfile').innerHTML], {
+            //type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
+            type: "excel"
+        });
+        saveAs(blob, "Profili.xls");
+    };
+
+    $scope.exportProfilePdf = function () {
+        html2canvas(document.getElementById('exportableProfile'), {
+            onrendered: function (canvas) {
+                var data = canvas.toDataURL();
+                var docDefinition = {
+                    content: [{
+                        image: data,
+                        width: 500,
+                    }]
+                };
+                pdfMake.createPdf(docDefinition).download("Profili.pdf");
+            }
+        });
+    };
+
+    $scope.modifica = function (row) {
+        $scope.profileRow = row;
+        $scope.newRow = {};
+        $scope.other2 = 'manager';
+        $scope.other3 = 'user';
+        if($scope.profileRow.profile == 'manager') {
+            $scope.other2 = 'admin';
+        }
+        if($scope.profileRow.profile == 'user') {
+            $scope.other3 = 'admin';
+        }
+
+        var myPopup = $ionicPopup.show({
+            templateUrl: 'templates/modificaPopup.html',
+            title: 'Modifica utente',
+            subTitle: 'Inserisci tutti i campi',
+            scope: $scope,
+            buttons: [
+                { text: 'Annulla' },
+                {
+                    text: '<b>Modifica</b>',
+                    type: 'button-positive',
+                    onTap: function(e) {
+                        if($scope.newRow.firstname && $scope.newRow.lastname  && $scope.newRow.profile != null) {
+                            e.preventDefault();
+                            $rootScope.users[$scope.profileRow.id].profile = $scope.newRow.profile;
+                            $rootScope.users[$scope.profileRow.id].firstname = $scope.newRow.firstname;
+                            $rootScope.users[$scope.profileRow.id].lastname = $scope.newRow.lastname;
+                            myPopup.close();
+                        }
+                        else {
+                            var alertPopup = $ionicPopup.alert({
+                                title: 'Alert',
+                                template: 'Nessuna modifica'
+                            });
+                            alertPopup.then(function(res) {
+                                // Custom functionality....
+                            });
+                        }
+                    }
+                }
+            ]
+        });
+
+        $timeout(function() {
+            myPopup.close(); //close the popup after 30 seconds for some reason
+        }, 30000);
+    };
+
+    $scope.addUser = function() {
+        $rootScope.newUser = {};
+        var myPopup = $ionicPopup.show({
+            templateUrl: 'templates/aggiungiUtentePopup.html',
+            title: 'Aggiungi nuovo utente',
+            subTitle: 'Inserisci tutti i campi',
+            scope: $scope,
+            buttons: [
+                { text: 'Annulla' },
+                {
+                    text: '<b>Aggiungi</b>',
+                    type: 'button-positive',
+                    onTap: function(e) {
+                        if($scope.newUser.firstname && $scope.newUser.lastname  && $scope.newUser.username  && $scope.newUser.password && $scope.newUser.profile != null) {
+                            e.preventDefault();
+                            //$rootScope.users.push($rootScope.newUser);
+                            $http.get("http://alessandroscarlato.it/aggiungiUtente.php")
+                                .then(function (response) {
+
+                                });
+
+
+
+
+                            myPopup.close();
+                        }
+                        else {
+                            var alertPopup = $ionicPopup.alert({
+                                title: 'Alert',
+                                template: 'Inserire tutti i campi'
+                            });
+                            alertPopup.then(function(res) {
+                                // Custom functionality....
+                            });
+                        }
+                    }
+                }
+            ]
+        });
+
+        $timeout(function() {
+            myPopup.close(); //close the popup after 60 seconds for some reason
+        }, 60000);
+
+    };
+
+});
